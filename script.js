49f2c32af41aa5abd2e03a6e66c56fff55bb7bf8
JAGANNATH PRASAD SAHU <143936739+JAGANNATH853@users.noreply.github.com>
2023-10-26 19:48:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,11 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+  count i = 0;
+	for(i = 0; i<library.length; i++){
+		library[i].readingStatus && count++;
+	}
+	return count;
 };
 
 // Do not change the code below
